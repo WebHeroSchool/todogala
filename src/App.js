b8eefec6219d0342a73026227222d585738029a5
Galina95 <u_galya_p@mail.ru>
2021-04-24 09:30:11 +0700

    new files index.js App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { render } from '@testing-library/react';
-import React, { Component } from 'react';
+import React from 'react';
 
-import {count, lenght} from './number.js';
+const element = (<div>
+  Hello World!
+  <a href="">go to</a>
+</div>)
 
-class App extends Component {
-  render() {
-    return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-          <div>
-            Общая длинна: {count*lenght}
-          </div>
-
-        <p style={{
-          color: 'red'
-        }}>
-          Hello World!
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-}
-
-export default App;
+export { element };
--- src/number.js
+++
@@ -1,4 +0,0 @@
-const count = 6;
-const lenght = 4;
-
-export { count, lenght};
