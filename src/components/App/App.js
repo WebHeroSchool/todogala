352e225b1fc27db2f5451c608591d56b74ff9875
Galina95 <u_galya_p@mail.ru>
2021-05-08 12:08:28 +0700

    new file App.js Footer.js Item.js ItemList.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ItemList from '../ItemList/ItemList';
 
+const todoItem = 'Написать новое приложение';
+
 const App = () => (
     <div>
         <h1>Важные дела:</h1>
-        <ItemList />
+        <InputItem />
+        <ItemList todoItem={todoItem} />
+        <Footer count={6} />
     </div>);
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Footer = ({ count }) => (<div>Остаось выполнить: {count}</div>);
+
+export default Footer;
