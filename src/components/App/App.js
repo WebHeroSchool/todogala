e42007f2b2b23d839ad8fea92d41cde62cad25b5
Galina95 <u_galya_p@mail.ru>
2021-05-17 16:49:01 +0700

    new file for key and map

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,27 @@ import Footer from '../Footer/Footer';
 
 const todoItem = 'Написать новое приложение';
 
-const App = () => (
+const App = () => 
+    const Items = [
+        {
+            value: 'Написать новое приложение' //0
+        },
+        {
+            value: 'Прописать props' //1
+        },
+        {
+            value: 'Сделать все дела' //2
+        }
+    ];
+
+    return (
     <div>
         <h1>Важные дела, которые необходимо выполнить:</h1>
         <InputItem />
         <ItemList todoItem={todoItem} />
         <Footer count={6} />
-    </div>);
+    </div>); 
+}
+    
 
 export default App;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Item from '../Item/Item';
 
-const ItemList = ({ todoItem }) => (<ul>
-    <li><Item todoItem={todoItem} /></li>
-    <li><Item todoItem={'прописать props'}/></li>
-    <li><Item todoItem={'сделать дела'}/></li>
+const ItemList = ({ items }) => (<ul>
+    {items.map(item => <li key={item.value}>
+        <Item value={item.value} />
+        </li>)}
 </ul>);
 
 export default ItemList;
