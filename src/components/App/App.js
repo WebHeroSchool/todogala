a14021e5acce8b49564ed5b2f865950a1648f0eb
Galina95 <u_galya_p@mail.ru>
2021-05-27 19:13:35 +0700

    new files for style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,9 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css'; 
 
+const taskCount = 5;
+document.body.style.margin = 0;
+
 const App = () => {
     const items = [
         {
@@ -22,12 +25,14 @@ const App = () => {
 
     return (
     <div className={styles.wrap}>
-        <h1 className={styles.title}>Важные дела, которые необходимо выполнить:</h1>
-        <InputItem />
-        <ItemList items={items} />
-        <Footer count={6} />
-    </div>); 
-}
+         <div className={styles.main}>
+            <h1 className={styles.title}>Список дел:</h1>
+            <InputItem />
+            <ItemList items={items} />
+            <Footer taskCount={taskCount} />
+        </div>
+    </div>)
+};
     
 
 export default App;
