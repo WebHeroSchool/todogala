e10b0406b964c5695c7beef4002db5c5bd208a45
Galina95 <u_galya_p@mail.ru>
2021-06-01 17:05:56 +0700

    new file for Component-class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,32 +7,33 @@ import styles from './App.module.css';
 const taskCount = 5;
 document.body.style.margin = 0;
 
-const App = () => {
-    const items = [
-        {
-            value: 'Написать новое приложение',
-            isDone: true
-        },
-        {
-            value: 'Прописать props',
-            isDone: false
-        },
-        {
-            value: 'Сделать все дела',
-            isDone: true
-        }
-    ];
-
+class App extends React.Component {
+    render() {
+        const items = [
+            {
+                value: 'Написать новое приложение',
+                isDone: true
+            },
+            {
+                value: 'Прописать props',
+                isDone: false
+            },
+            {
+                value: 'Сделать все дела',
+                isDone: true
+            }
+        ];
+        
     return (
-    <div className={styles.wrap}>
+        <div className={styles.wrap}>
          <div className={styles.main}>
             <h1 className={styles.title}>Список дел:</h1>
             <InputItem />
             <ItemList items={items} />
             <Footer taskCount={taskCount} />
         </div>
-    </div>)
-};
-    
+    </div>);
+    }
+}
 
 export default App;
